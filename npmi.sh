8a0428b578cbd527543283c14694ecbb31988583
Gaurav.Gaur <gaurav.gaur@ocado.com>
2021-03-06 19:04:21 +0000

    refresh on Sat  6 Mar 19:04:21 GMT 2021

---
+++ npmi.sh
@@ -0,0 +1,9 @@
+cd /home/gauravgaur/1My/study/projects/gaur4vgaur.github.io
+git pull
+rm -r *
+rsync -a /home/gauravgaur/1My/study/projects/blog/blog-starter/ /home/gauravgaur/1My/study/projects/gaur4vgaur.github.io/ --exclude .git --exclude .git --exclude .cache --exclude .idea --exclude node_modules --exclude public --exclude ContentRefresherInGitio.py
+npm install && npm i gatsby-image && npm i dompurify
+git add .
+git commit -m "refresh on $(date)"
+git push origing master
+npm run deploy
